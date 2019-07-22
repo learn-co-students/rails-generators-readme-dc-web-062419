b57ef1aba2ae79f03cf7a3f10533bb84acfe83f1
github username <github email address>
2019-07-22 14:05:57 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'categories/show'
+  resources :accounts, only: [:new, :create,
+    :edit, :update, :destroy, :index, :show,]
+  
+    resources :admin, only: [:dashboard, 
+      :stats, :financials, :settings]
+    resources :posts, only: [:index, 
+      :show, :new, :create, :edit, 
+      :update]
+    resources :categories, only: [:show]
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  resources :categories
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  
 end
